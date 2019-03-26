23b620cb153d13ea0df10d5a578503f104640ad3
Codey Pinero <codey.pinero@gmail.com>
2019-03-26 15:35:14 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,8 +3,14 @@ def second_challenge
     :montague => {
       :patriarch => {},
       :matriarch => {},
-      :hero => {}
+      :hero => {},
+      :hero_friends => []
     },
-    :capulet => {}
+    :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+    }
 }
 end
