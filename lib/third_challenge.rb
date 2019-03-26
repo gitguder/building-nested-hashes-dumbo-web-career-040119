81cc6cfbe7e6f97de39aac40496cde51b57827ce
Codey Pinero <codey.pinero@gmail.com>
2019-03-26 15:41:47 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,8 +7,8 @@ def third_challenge
       :hero_friends => []
     },
     :capulet => {
-      :patriarch => { :name => "Lord Capulet", :age => "51" },
-      :matriarch => {},
+      :patriarch => { :name => "Lord Capulet", :age => "50" },
+      :matriarch => { :name => "Lady Capulet", :age => "51" },
       :heroine => {},
       :heroine_friends => []
     }
